41332fdb46573187b30e16e5c1d77ed09fec571e
wbridgri <will.bridges@comcast.net>
2025-02-23 09:31:01 -0600

    redid everything independently while consulting notes.  will probably do again

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
-let count = 1
+/**
+ * 
+ * 
+ * 
+ * guitarName
+ * bodyType
+ * scaleLength
+ * numFrets
+ * pickup
+ * 
+ * submitBtn
+ * 
+ * cardRow
+ * 
+ */
+
+//First pass.  I redid the HTML and tried to be original when writing this.  I had to consult the animalsjs file for the querySelector thing.  I'm having trouble getting the options to display properly.  They display the value when I want them to display the contents between the option tag.  I saw that one way I could do this was to not give the option elements value attributes; but I think that's not good for screen readers.
+
 
 class Guitar {
     constructor(guitarName, bodyType, scaleLength, numFrets, pickup) {
-        this.info = {
-            id: count,
+        this.data = {
             guitarName,
             bodyType,
             scaleLength,
@@ -12,87 +28,81 @@ class Guitar {
         }
     }
 
-    getDescription(guitarName, bodyType, scaleLength, numFrets, pickup) {
-        return `The ${guitarName} is a ${bodyType} guitar.  It has a scale length of ${scaleLength} inches, ${numFrets} frets and ${pickup} pickups.`
-    }
+
 }
+
 
 const submitBtn = document.getElementById('submitBtn')
 
-submitBtn.addEventListener('click', (e)=> {
+submitBtn.addEventListener('click' , (e) => {
     e.preventDefault()
 
-    let obj = {};
-    obj = {...getInfo()}
+    
 
-    const guitar = new Guitar(obj.guitarName, obj.bodyType, obj.scaleLength, obj.numFrets, obj.pickups)
-    count++
+    const guitarName = document.getElementById('guitarName').value
+    const bodyType = document.getElementById('bodyType').value
+    const numFrets = document.getElementById('numFrets').value
+    const scaleLength = document.querySelector('input[name="scaleLength"]:checked').value 
+    const pickup = document.querySelectorAll('input[name="pickup"]:checked')
 
-    buildCard(guitar.info, guitar.getDescription)
+    const pickups = [];
+
+    document.querySelectorAll('input[type=checkbox]:checked').forEach((node) => {
+        pickups.push(node.value);
+    });  //saw this on stack exchange
+
+
+    const guitar = new Guitar(guitarName, bodyType, scaleLength, numFrets, pickups)
+
+    buildCards(guitar.data)
+    console.log(guitar.data)
 
 })
 
-const getInfo = () => {
-    const guitarName = document.getElementById('guitarName').value
-    const bodyType = document.getElementById('bodyType').value
-    const scaleLength = document.querySelector('input[name="scaleLength"]:checked').value
-    const numFrets = document.getElementById('numFrets').value
-    const pickup = document.querySelectorAll('input[name="pickup"]')
-
-    let pickups = [];
-
-    pickup.forEach(p => {
-        p.checked ? pickups = [...pickups, p.value] : null
-    })
-
-    return { guitarName, bodyType, scaleLength, numFrets, pickups }
 
 
-}
 
-const buildCard = (obj, func)=> {
-    const row = document.getElementById('cardRow')
 
+
+const buildCards = (data) => {
+    const cardRow = document.getElementById('cardRow')
     const col = document.createElement('div')
     col.classList.add('col')
 
     const card = document.createElement('div')
-    card.classList.add('card', 'h-100')
+    card.classList.add('card')
 
-    const cardBody = document.createElement('section')
+    const cardBody = document.createElement('div')
     cardBody.classList.add('card-body')
 
     const guitarName = document.createElement('h2')
     guitarName.classList.add('card-title', 'text-capitalize')
-    guitarName.innerText = obj.guitarName
+    guitarName.innerText = data.guitarName
+
 
     const bodyType = document.createElement('p')
     bodyType.classList.add('card-text', 'text-capitalize')
-    bodyType.innerText = `Body Type: ${obj.bodyType}`
-
-    const scaleLength = document.createElement('p')
-    scaleLength.classList.add('card-text')
-    scaleLength.innerText = `Scale Length: ${obj.scaleLength}`
+    bodyType.innerText = data.bodyType
+    // bodyType.innerText.includes('semiHollow') ? bodyType.innerText = `${data.bodyType} Body` : bodyType.innerText = data.bodyType  attempts to fix problem but doesnt
 
     const numFrets = document.createElement('p')
     numFrets.classList.add('card-text')
-    numFrets.innerText = `Number of Frets: ${obj.numFrets}`
+    numFrets.innerText = `${data.numFrets} frets`
 
-    const description = document.createElement('p')
-    description.classList.add('card-text')
-    description.innerText = func(obj.guitarName, obj.bodyType, obj.scaleLength, obj.numFrets, obj.pickup)
+    const scaleLength = document.createElement('p')
+    scaleLength.classList.add('card-text')
+    scaleLength.innerText = data.scaleLength
+    scaleLength.innerText !== 'other' ? scaleLength.innerText = `${data.scaleLength} inches` : scaleLength.innerText = data.scaleLength
+
+    const pickup = document.createElement('p')
+    pickup.classList.add('card-text')
+    pickup.innerText =`${data.pickup}`
 
     cardBody.appendChild(guitarName)
     cardBody.appendChild(bodyType)
-    cardBody.appendChild(scaleLength)
     cardBody.appendChild(numFrets)
-    cardBody.appendChild(description)
-
+    cardBody.appendChild(scaleLength)
+    cardBody.appendChild(pickup)
     card.appendChild(cardBody)
     col.appendChild(card)
-    row.appendChild(col)
-
-
-}
-
-//Look I did this because I have to work tonight and if I don't I'm going to be fired
+    cardRow.appendChild(col)
